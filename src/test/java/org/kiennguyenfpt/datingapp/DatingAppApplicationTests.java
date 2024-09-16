56ac0ca946071498c975af9de4587c00508a1382
NGUYEN CAO TRUNG KIEN <kiennguyenfpt2711@gmail.com>
2024-09-16 09:31:14 +0700

    Initilization project

---
+++ src/test/java/org/kiennguyenfpt/datingapp/DatingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kiennguyenfpt.datingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

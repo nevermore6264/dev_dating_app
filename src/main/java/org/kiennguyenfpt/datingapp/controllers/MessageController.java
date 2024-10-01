b8ca07141a7f3eab9c5d876a59a84b76065f3297
doanh <nguyentrongdoanh202@gmail.com>
2024-10-01 21:57:42 +0700

    go commit Message de fix loi

--- src/main/java/org/kiennguyenfpt/datingapp/controllers/MessageController.java
+++ src/main/java/org/kiennguyenfpt/datingapp/controllers/MessageController.java
@@ -1,53 +1,53 @@
-package org.kiennguyenfpt.datingapp.controllers;
-
-import org.kiennguyenfpt.datingapp.dtos.requests.MessageRequest;
-import org.kiennguyenfpt.datingapp.entities.Message;
-import org.kiennguyenfpt.datingapp.responses.CommonResponse;
-import org.kiennguyenfpt.datingapp.services.MessageService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("api/v1/messages")
-public class MessageController {
-    private final MessageService messageService;
-
-    public MessageController(MessageService messageService) {
-        this.messageService = messageService;
-    }
-
-    @PostMapping("/send")
-    public ResponseEntity<CommonResponse<Message>> sendMessage(@RequestBody MessageRequest messageRequest) {
-        CommonResponse<Message> response = new CommonResponse<>();
-        try {
-            Message message = messageService.sendMessage(messageRequest.getSenderId(), messageRequest.getReceiverId(), messageRequest.getContent());
-            response.setStatus(HttpStatus.OK.value());
-            response.setMessage("Message sent successfully.");
-            response.setData(message);
-            return ResponseEntity.ok(response);
-        } catch (Exception e) {
-            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            response.setMessage("Error sending message: " + e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
-        }
-    }
-
-    @GetMapping("/between/{userId1}/{userId2}")
-    public ResponseEntity<CommonResponse<List<Message>>> getMessages(@PathVariable Long userId1, @PathVariable Long userId2) {
-        CommonResponse<List<Message>> response = new CommonResponse<>();
-        try {
-            List<Message> messages = messageService.getMessages(userId1, userId2);
-            response.setStatus(HttpStatus.OK.value());
-            response.setMessage("Messages retrieved successfully.");
-            response.setData(messages);
-            return ResponseEntity.ok(response);
-        } catch (Exception e) {
-            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            response.setMessage("Error retrieving messages: " + e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
-        }
-    }
-}
+//package org.kiennguyenfpt.datingapp.controllers;
+//
+//import org.kiennguyenfpt.datingapp.dtos.requests.MessageRequest;
+//import org.kiennguyenfpt.datingapp.entities.Message;
+//import org.kiennguyenfpt.datingapp.responses.CommonResponse;
+//import org.kiennguyenfpt.datingapp.services.MessageService;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("api/v1/messages")
+//public class MessageController {
+//    private final MessageService messageService;
+//
+//    public MessageController(MessageService messageService) {
+//        this.messageService = messageService;
+//    }
+//
+//    @PostMapping("/send")
+//    public ResponseEntity<CommonResponse<Message>> sendMessage(@RequestBody MessageRequest messageRequest) {
+//        CommonResponse<Message> response = new CommonResponse<>();
+//        try {
+//            Message message = messageService.sendMessage(messageRequest.getSenderId(), messageRequest.getReceiverId(), messageRequest.getContent());
+//            response.setStatus(HttpStatus.OK.value());
+//            response.setMessage("Message sent successfully.");
+//            response.setData(message);
+//            return ResponseEntity.ok(response);
+//        } catch (Exception e) {
+//            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+//            response.setMessage("Error sending message: " + e.getMessage());
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+//        }
+//    }
+//
+//    @GetMapping("/between/{userId1}/{userId2}")
+//    public ResponseEntity<CommonResponse<List<Message>>> getMessages(@PathVariable Long userId1, @PathVariable Long userId2) {
+//        CommonResponse<List<Message>> response = new CommonResponse<>();
+//        try {
+//            List<Message> messages = messageService.getMessages(userId1, userId2);
+//            response.setStatus(HttpStatus.OK.value());
+//            response.setMessage("Messages retrieved successfully.");
+//            response.setData(messages);
+//            return ResponseEntity.ok(response);
+//        } catch (Exception e) {
+//            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+//            response.setMessage("Error retrieving messages: " + e.getMessage());
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+//        }
+//    }
+//}
--- src/main/java/org/kiennguyenfpt/datingapp/services/MessageService.java
+++ src/main/java/org/kiennguyenfpt/datingapp/services/MessageService.java
@@ -1,10 +1,10 @@
-package org.kiennguyenfpt.datingapp.services;
-
-import org.kiennguyenfpt.datingapp.entities.Message;
-
-import java.util.List;
-
-public interface MessageService {
-    Message sendMessage(Long senderId, Long receiverId, String content);
-    List<Message> getMessages (Long userId1, Long userId2);
-}
+//package org.kiennguyenfpt.datingapp.services;
+//
+//import org.kiennguyenfpt.datingapp.entities.Message;
+//
+//import java.util.List;
+//
+//public interface MessageService {
+//    Message sendMessage(Long senderId, Long receiverId, String content);
+//    List<Message> getMessages (Long userId1, Long userId2);
+//}

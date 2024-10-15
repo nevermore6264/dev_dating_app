ebd84fffaec1fe5439077b2a0f937aa94336e9fd
David <nevermore6264@example.com>
2024-10-16 03:07:32 +0700

    [Dating-App] Update source code

--- frontend/src/views/GetLocation.vue
+++ frontend/src/views/GetLocation.vue
@@ -1,21 +1,57 @@
 <template>
-    <div class="container text-center">
-      <p class="mb-0 text-light">
-        © {{ currentYear }} LoveBell. – Made with ❤ in Ho Chi Minh City
-      </p>
+  <div class="emergency-page-container">
+    <!-- Sidebar -->
+    <LoveBellSidebar />
+
+    <!-- Nội dung chính của trang -->
+    <div class="emergency-page">
+      <!-- Tiêu đề trang -->
+      <div class="header">
+        <h1>GET CURRENT LOCATION</h1>
+        <button class="emergency-button">Get Location</button>
+      </div>
     </div>
+  </div>
 </template>
 
-<script setup>
+<script>
+import LoveBellSidebar from "@/views/sidebar/LoveBellSidebar.vue";
 
-// Lấy năm hiện tại
-const currentYear = new Date().getFullYear();
+export default {
+  components: {
+    LoveBellSidebar,
+  },
+};
 </script>
 
 <style scoped>
-footer {
-  position: absolute;
-  width: 100%;
-  bottom: 0;
+/* Container Flexbox để chứa sidebar và phần chính */
+.emergency-page-container {
+  display: flex;
+  align-items: flex-start;
+}
+
+.emergency-page {
+  flex: 1;
+  padding: 20px;
+  text-align: center;
+  font-family: Arial, sans-serif;
+}
+
+.header h1 {
+  color: #ff33cc;
+  font-size: 48px;
+  font-weight: bold;
+  margin: 10px 0;
+}
+
+.emergency-button {
+  background-color: #ff0000;
+  border: 2px solid #000;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  color: #fff;
+  font-weight: bold;
 }
 </style>

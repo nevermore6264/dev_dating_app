9a7252bf03e630396c4b15b8eb7ee130d35c4064
doanh <nguyentrongdoanh202@gmail.com>
2024-10-17 02:37:58 +0700

    update tbl UserPreference

--- backend/src/main/java/org/kiennguyenfpt/datingapp/entities/UserPreference.java
+++ backend/src/main/java/org/kiennguyenfpt/datingapp/entities/UserPreference.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 @Table(name = "UserPreferences")
 public class UserPreference {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long preferenceId;
 
     @ManyToOne

6d050c1b0ac0d07237107afe324c93c7bb255642
NGUYEN CAO TRUNG KIEN <kiennguyenfpt2711@gmail.com>
2024-10-16 23:55:25 +0700

    16/10 upppppp

---
+++ backend/src/main/java/org/kiennguyenfpt/datingapp/dtos/responses/LoginSuccessfulResponse.java
@@ -0,0 +1,18 @@
+package org.kiennguyenfpt.datingapp.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginSuccessfulResponse {
+    private String email;
+
+    private String token;
+
+    private String role;
+}

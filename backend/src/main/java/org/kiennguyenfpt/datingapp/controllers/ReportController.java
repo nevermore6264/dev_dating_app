192a7fd7fede13e51bc25ef74f15c523c484345c
NGUYEN CAO TRUNG KIEN <kiennguyenfpt2711@gmail.com>
2024-10-16 23:36:39 +0700

    16/10 upppppp

--- backend/src/main/java/org/kiennguyenfpt/datingapp/controllers/ReportController.java
+++ backend/src/main/java/org/kiennguyenfpt/datingapp/controllers/ReportController.java
@@ -4,7 +4,11 @@ import org.kiennguyenfpt.datingapp.responses.CommonResponse;
 import org.kiennguyenfpt.datingapp.services.ReportService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/report")
@@ -46,4 +50,5 @@ public class ReportController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
     }
+
 }

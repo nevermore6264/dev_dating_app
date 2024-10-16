4d539b39095fff09f49198650173c3804362fb8d
doanh <nguyentrongdoanh202@gmail.com>
2024-10-17 02:37:49 +0700

    update tbl UserRole

--- backend/src/main/java/org/kiennguyenfpt/datingapp/entities/UserRole.java
+++ backend/src/main/java/org/kiennguyenfpt/datingapp/entities/UserRole.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 @Table(name = "UserRoles")
 public class UserRole {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long userRoleId;
 
     @ManyToOne

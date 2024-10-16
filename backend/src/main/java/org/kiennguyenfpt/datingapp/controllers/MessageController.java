82bc018ceedd6a695b4e6f41878f9d8e35b9c0cd
doanh <nguyentrongdoanh202@gmail.com>
2024-10-16 23:44:58 +0700

    update message

--- backend/src/main/java/org/kiennguyenfpt/datingapp/controllers/MessageController.java
+++ backend/src/main/java/org/kiennguyenfpt/datingapp/controllers/MessageController.java
@@ -11,21 +11,23 @@ import org.kiennguyenfpt.datingapp.services.MessageService;
 import org.kiennguyenfpt.datingapp.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/messages")
 @CrossOrigin
-
 public class MessageController {
     private final MessageService messageService;
     private final MatchService matchService;
@@ -135,4 +137,5 @@ public class MessageController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
     }
+
 }

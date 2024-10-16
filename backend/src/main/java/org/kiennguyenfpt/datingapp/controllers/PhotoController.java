f2b965c918452e2e34e2e7a0ecd022e431d74e8c
NGUYEN CAO TRUNG KIEN <kiennguyenfpt2711@gmail.com>
2024-10-16 23:34:29 +0700

    16/10 upppppp

--- backend/src/main/java/org/kiennguyenfpt/datingapp/controllers/PhotoController.java
+++ backend/src/main/java/org/kiennguyenfpt/datingapp/controllers/PhotoController.java
@@ -8,15 +8,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/photos")
 @CrossOrigin
-
 public class PhotoController {
     private final PhotoService photoService;
     private final UserRepository userRepository;
@@ -76,5 +80,5 @@ public class PhotoController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
     }
+    
 }
-

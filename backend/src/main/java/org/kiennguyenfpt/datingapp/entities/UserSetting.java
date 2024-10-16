b7b2baafe0dc0f37b6d3b3b31ae038f3f2e56f1b
doanh <nguyentrongdoanh202@gmail.com>
2024-10-17 02:37:36 +0700

    update tbl UserSetting

--- backend/src/main/java/org/kiennguyenfpt/datingapp/entities/UserSetting.java
+++ backend/src/main/java/org/kiennguyenfpt/datingapp/entities/UserSetting.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 @Table(name = "UserSettings")
 public class UserSetting {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long settingId;
 
     @ManyToOne

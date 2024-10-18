7d8653232012932f4bffca90be142194b1b51046
DucHD3 <hoangduc22k2002@gmail.com>
2024-10-19 00:56:47 +0700

    create management

--- frontend/src/views/sidebar/LoveBellSidebar.vue
+++ frontend/src/views/sidebar/LoveBellSidebar.vue
@@ -61,6 +61,14 @@
         </router-link>
       </li>
 
+      <!-- Link to Safety -->
+      <li class="menu-item">
+        <router-link to="/getLocation" class="menu-link">
+          <i class="material-icons">location_on</i>
+          <span>Location</span>
+        </router-link>
+      </li>
+
       <!-- Profile Link -->
       <li class="menu-item profile">
         <router-link to="/profile" class="menu-link">
@@ -70,7 +78,7 @@
       </li>
 
       <!-- Link to More Options (Dropdown) -->
-      <li class="menu-item" @click="toggleMoreOptions">
+      <li class="menu-item see-more" @click="toggleMoreOptions">
         <i class="material-icons">menu</i>
         <span>Xem thêm</span>
       </li>
@@ -151,13 +159,13 @@ export default {
 <style>
 .lovebell-sidebar {
   background-color: #ff85a1;
-  height: auto;
   width: 300px;
   color: #000;
   font-family: Arial, sans-serif;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  height: calc(100vh - 56px);
 }
 
 .menu {
@@ -170,8 +178,18 @@ export default {
   padding: 15px 20px;
   font-size: 20px;
   font-weight: bold;
-  display: flex;
-  align-items: center;
+  display: block;
+  width: 100%;
+  cursor: pointer;
+  color: #000;
+}
+
+.profile, .see-more {
+  padding: 15px 20px;
+  font-size: 20px;
+  font-weight: bold;
+  display: block;
+  width: 30%;
   cursor: pointer;
   color: #000;
 }
@@ -207,8 +225,23 @@ export default {
   margin-right: 15px;
 }
 
-.profile {
-  margin-top: 260px;
+@media screen and (max-height: 764px) {
+  .lovebell-sidebar{
+    overflow-y: scroll;
+  }
+}
+
+
+@media screen and (min-height: 764px) {
+  .profile {
+    position: absolute;
+    bottom: 112px;
+  }
+
+  .see-more {
+    position: absolute;
+    bottom: 56px;
+  }
 }
 
 .material-icons {
@@ -238,9 +271,9 @@ export default {
 /* Cách dropdown hiển thị từ dưới lên */
 .dropdown-reverse {
   position: absolute;
-  transform: translateY(-125%); /* Thả lên trên */
+  bottom: 260px;
   background-color: #fff;
-  width: 17%;
+  width: 260px;
   list-style-type: none;
   box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
   border-radius: 20px;
@@ -255,9 +288,5 @@ export default {
 
 .dropdown-reverse .dropdown-item:hover {
   background-color: rgba(255, 255, 255, 0.1);
-}
-
-.menu-item:last-child {
-  margin-bottom: 20px;
 }
 </style>

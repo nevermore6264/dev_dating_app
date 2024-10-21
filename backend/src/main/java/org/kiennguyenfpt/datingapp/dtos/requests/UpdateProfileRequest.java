eb428c9407d13552dea4b35a60d34f0d0e92004f
NGUYEN CAO TRUNG KIEN <kiennguyenfpt2711@gmail.com>
2024-10-22 02:06:42 +0700

    save before pull

--- backend/src/main/java/org/kiennguyenfpt/datingapp/dtos/requests/UpdateProfileRequest.java
+++ backend/src/main/java/org/kiennguyenfpt/datingapp/dtos/requests/UpdateProfileRequest.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Getter
 @Setter
 public class UpdateProfileRequest {
-    /*
+
     private List<MultipartFile> files;
     @NotBlank(message = "Name is mandatory")
     private String name;
@@ -35,5 +35,5 @@ public class UpdateProfileRequest {
     @Pattern(regexp = "^\\+?[0-9. ()-]{7,25}$", message = "Phone number is invalid")
     private String phone;
 
-     */
+
 }

7d680034b1dbd8c635f56c862588e23cc5299215
lqhuy28 <lequanghuy2809@gmail.com>
2024-10-24 13:03:37 +0700

    api get all package

---
+++ backend/src/main/java/org/kiennguyenfpt/datingapp/services/PackageService.java
@@ -0,0 +1,10 @@
+package org.kiennguyenfpt.datingapp.services;
+
+import org.kiennguyenfpt.datingapp.entities.Package;
+
+import java.util.List;
+
+public interface PackageService {
+
+    List<Package> getAllPackages();
+}

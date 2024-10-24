b33d97b9c2e7ebbeaf1bd4991ebc519c1b1b9d39
doanh <nguyentrongdoanh202@gmail.com>
2024-10-24 12:44:05 +0700

    PaymentService

--- backend/src/main/java/org/kiennguyenfpt/datingapp/services/PaymentService.java
+++ backend/src/main/java/org/kiennguyenfpt/datingapp/services/PaymentService.java
@@ -1,5 +1,11 @@
 package org.kiennguyenfpt.datingapp.services;
 
+import org.kiennguyenfpt.datingapp.dtos.responses.PaymentResponse;
+
+import java.util.List;
+
 public interface PaymentService {
     void processPayment(Long userId, Double amount);
+
+    List<PaymentResponse> getPaymentHistory(Long userId);
 }

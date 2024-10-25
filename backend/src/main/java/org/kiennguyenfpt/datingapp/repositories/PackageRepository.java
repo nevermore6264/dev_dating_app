8ae9a59f597ce6722ef28f6a8d9778876161817c
doanh <nguyentrongdoanh202@gmail.com>
2024-10-25 14:22:18 +0700

    delete package

--- backend/src/main/java/org/kiennguyenfpt/datingapp/repositories/PackageRepository.java
+++
@@ -1,9 +0,0 @@
-package org.kiennguyenfpt.datingapp.repositories;
-
-import org.kiennguyenfpt.datingapp.entities.Package;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PackageRepository extends JpaRepository<Package, Long> {
-}

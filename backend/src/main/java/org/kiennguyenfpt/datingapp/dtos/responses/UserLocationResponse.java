3257f1360ebf8ae7e313667eb3937097c3f87405
doanh <nguyentrongdoanh202@gmail.com>
2024-10-29 00:53:48 +0700

    update src code

---
+++ backend/src/main/java/org/kiennguyenfpt/datingapp/dtos/responses/UserLocationResponse.java
@@ -0,0 +1,18 @@
+package org.kiennguyenfpt.datingapp.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLocationResponse {
+    private Double latitude;
+
+    private Double longitude;
+
+    private String address;
+}

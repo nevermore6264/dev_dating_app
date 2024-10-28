1585e46c581f1a4b65ffc99679ee641631b6e612
doanh <nguyentrongdoanh202@gmail.com>
2024-10-29 00:53:31 +0700

    update src code

--- backend/src/main/java/org/kiennguyenfpt/datingapp/services/UserSubscriptionService.java
+++ backend/src/main/java/org/kiennguyenfpt/datingapp/services/UserSubscriptionService.java
@@ -1,8 +1,27 @@
 package org.kiennguyenfpt.datingapp.services;
 
-import org.kiennguyenfpt.datingapp.dtos.responses.SubscriptionPlanResponse;
-import org.kiennguyenfpt.datingapp.entities.UserSubscription;
+import org.kiennguyenfpt.datingapp.dtos.responses.AdminUserSubscriptionResponse;
+import org.kiennguyenfpt.datingapp.dtos.responses.AdminUserWithSubscriptionDetails;
+import org.kiennguyenfpt.datingapp.dtos.responses.RevenueStatsResponse;
+import org.kiennguyenfpt.datingapp.dtos.responses.SubscriptionStatsResponse;
+
+import java.util.List;
 
 public interface UserSubscriptionService {
-    SubscriptionPlanResponse getActiveSubscriptionByUserId(Long userId);
+
+    List<AdminUserSubscriptionResponse> getAllUserSubscriptions();
+
+    AdminUserWithSubscriptionDetails getUserSubscriptionById(Long id);
+
+    List<SubscriptionStatsResponse> getPackageCountByMonth();
+
+    List<SubscriptionStatsResponse> getPackageCountByQuarter();
+
+    List<SubscriptionStatsResponse> getPackageCountByYear();
+
+    List<RevenueStatsResponse> getMonthlyRevenue();
+
+    List<RevenueStatsResponse> getQuarterlyRevenue();
+
+    List<RevenueStatsResponse> getYearlyRevenue();
 }
